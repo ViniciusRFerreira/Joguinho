a75b9fff755f6d91a3dcf1a8820f197cde3a358b
Vinicius <101262773+ViniciusRFerreira@users.noreply.github.com>
2022-06-28 22:27:23 -0300

    Add files via upload
    finished

--- ex6_2.js
+++ ex6_2.js
@@ -1,35 +1,56 @@
-const frm = document.querySelector('inNumero')
-const num = Math.random() * 100;
-const respNome = document.querySelector('outErros')
-const respLista = document.querySelector('outChances')
+let num = Math.floor(Math.random() * 100);
+console.log(num);
+let erro = [];
 
-const tentativas = []
+const frm = document.querySelector("form");
+let outDica = document.getElementById("outDica");
+let outErro = document.getElementById("outErros");
+let outChance = document.getElementById("outChances");
+let inNumero = document.getElementById("inNumero");
 
-frm.addEventListener('submit', (e) => {
-    e.preventDefault() //formulario nao enviar dados
-    const val = frm.inNumero.number //obtem o nome do paciente
-    tentativas.push(val) //adiciona elemento no final do vetor
-    console.log(tentativas);
-    let lista=''
-    for(let i = 0; i < tentativas.length; i++){
-        lista+= lista + "; "
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let tentativas = Number(frm.inNumero.value);
+
+  if (tentativas != num) {
+
+    if (erro.length > 5) {
+      outErro.textContent = `${erro.length}: (${erro})`;
+      outChance.textContent = 6 - erro.length;
+      outDica.textContent = `Você perdeu! O número certo era: ${num}`;
+      frm.btNovo.setAttribute("class", "exibe");
+      return;
     }
-    if (val == num){
-        //respLista.innerText = "Parabéns você acertou!"
-    }
-    if (val < num){
-        //= "O número é maior do que o digitado."
-    }
-    if (val > num){
-        //= "O número é menor do que o digitado."
-    }
-    if (val != num){
-        //erros = erros +1
-    }
+    erro.push(tentativas);
+    outErro.textContent = `${erro.length}: (${erro})`;
+    outChance.textContent = 6 - erro.length;
+    inNumero.value = inNumero.defaultValue;
     
-    respLista.innerText = lista //exiba a lista de pacientes
-    frm.inNumero.value = ''
-    frm.inNumero.focus() //posiciona o cursor no campo
+    if (tentativas > num) {
+      inNumero.setAttribute("max", tentativas);
+    } else {
+      inNumero.setAttribute("min", tentativas);
+    }
+    outDica.textContent = `O número é maior que ${inNumero.getAttribute("min")} e menor que ${inNumero.getAttribute("max")}.`;
 
-    
-})
+  } else if (erro.length < 6) {
+    outDica.textContent = `Você acertou o número: ${num}. Parabéns!`;
+    frm.btNovo.setAttribute("class", "exibe");
+    return;
+  }
+
+});
+
+frm.btNovo.addEventListener("click", () => {
+  console.log("Botão jogar novamente clicado");
+  erro = [];
+  outDica.textContent = outDica.defaultValue;
+  outErro.textContent = 0;
+  outChance.textContent = 6;
+  inNumero.setAttribute("max", 100);
+  inNumero.setAttribute("min", 0);
+  inNumero.value = inNumero.defaultValue;
+  num = Math.floor(Math.random() * 100); 
+  console.log(num);
+  frm.btNovo.setAttribute("class", "oculta");
+});
